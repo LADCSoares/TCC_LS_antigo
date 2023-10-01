c5adb7674e117d4df7b11f6b41d0c4e5cdcc8c94
Luisa Andrielly <145121607+LADCSoares@users.noreply.github.com>
2023-10-01 18:31:18 -0300

    Create deletarFun.php

---
+++ deletarFun.php
@@ -0,0 +1,15 @@
+<?php
+include("conecta.php");
+
+   $id = $_GET['idFuncionario'];
+   $sql = "SELECT * FROM funcionario WHERE idFuncionario=$id";
+   $resultado = mysqli_query($conexao, $sql);
+   $linha = mysqli_fetch_array($resultado);
+   
+
+$sql = "DELETE FROM funcionario where idFuncionario=$id";
+$resultado = mysqli_query($conexao, $sql);
+
+// arrumar o deletar
+header("Location: consultar.php");
+?>
