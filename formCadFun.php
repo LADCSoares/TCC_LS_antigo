f87b18ef97d428480722be50c46b0ccbec61f3d4
Luisa Andrielly <145121607+LADCSoares@users.noreply.github.com>
2023-10-01 23:17:41 -0300

    Update formCadFun.php
    materialize do formCadFun

--- formCadFun.php
+++ formCadFun.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="Content-Type" content="text/html"/>
-  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-   		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        
-
+<meta charset="UTF-8">
+        <!--Import Google Icon Font-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lilita+One|Roboto">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <!--Import materialize.css-->
         <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 	<title>SCAEM</title>
+	<style>
+    .cont1{
+		font-family: "Roboto", sans-serif;
+		background-color: rgb(8,83,148);
+	}
+
+	.topicos{
+		font-family: "Roboto", sans-serif;
+		font-size: 18px;
+		margin-left: 46px;
+		font-weight: 500;
+	}
+	</style>
 </head>
 <body>
 
@@ -25,38 +37,47 @@
   require_once "topo.html"
 
     ?>
-    <br>
+
 
 	<div class="row">
-		<div class="col s7 offset-s4">
+	
+		<div class="container z-depth-3 col s6 offset-s4 cont1">
+		<h4 class="center-align white-text"> Cadastrar Funcionário </h4>
+		</div>
+		
+		<div class="col s8 offset-s3">
 
-			<div class="container">
-
-			<h3 class="center-align"> Cadastrar Funcionário </h3>
+			<div class="container ">
+			<br>
+			<div class="col s10 offset-s1">
 				<form method="POST" action="cadFuncionario.php">
-
+                    <p class="topicos">Nome:</p>
 					<div class="input-field">
 						<i class="material-icons prefix">perm_identity</i>
-						<label for="namo">Nome </label>
+						<label for="name"> </label>
 						<input type="text" name="nome" required>
 					</div>
 
+					<p class="topicos">CPF:</p>
 					<div class="input-field">
 						<i class="material-icons prefix ">email</i>
-						<label for="cpf">CPF</label>
+						<label for="cpf"></label>
 						<input type="number" name="cpf" required>
 					</div>
-
+					
+					<p class="topicos">Cargo:</p>
           			<div class="input-field">
 						<i class="material-icons prefix">person_pin</i>
-						<label for="cargo">Cargo</label>
+						<label for="cargo"></label>
 						<input type="text" name="cargo" required>
 					</div>
 
+					<p class="topicos">Telefone:</p>
 					<div class="input-field">
+				
 						<i class="material-icons prefix ">email</i>
-						<label for="telefone">telefone</label>
-						<input type="number" name="telefone" required>
+						<label for="telefone"></label>
+						<input type="tel" name="telefone" placeholder="(+55)99999-9999" required>
 					</div>
 
 				
@@ -65,16 +86,15 @@
 					</p>
 
 				</form>
-
+              </div>
 			</div>
 		</div>
 	</div>
 
 
-			
 
 
-    </form>
+
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
 
