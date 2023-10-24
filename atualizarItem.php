cb26efa279289a7aba73278f05a43131a802485d
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-10-24 16:52:51 -0300

    Update atualizarItem.php

--- atualizarItem.php
+++ atualizarItem.php
@@ -2,18 +2,27 @@
 include("conecta.php");
 // faz conexao com o banco
 
-$id = $_POST["idItem"];
-$tipo = $_POST["tipo"];
+$tipo = $_POST["nomeItem"];
 $descricao = $_POST["descricao"];
+$almoxarifado = $_POST["almoxarifado"];
 // var_dump($cpf);
 
 // var_dump($telefone);
 // $id_Item = "";
 
-$sql = "UPDATE funcionario SET tipo = '$tipo', descricao = '$descricao' WHERE idItem =  $id";
+$sql = "UPDATE funcionario SET tipo = '$tipo', descricao = '$descricao', almoxarifado = '$almoxarifado' WHERE idItem =  $id";
 $resultado = mysqli_query($conexao, $sql);
-if ($resultado == true) {
-    header("Location: consultar.php?result=true");
-} else {
-    header("Location: consultar.php?result=false");
+
+if($almoxarifado == 1){
+    if ($resultado == true) {
+        header("Location: consultarGeral.php?result=alert");
+    } else {
+        header("Location: formCadItem.php?result=false");
+    }
+}else{
+    if ($resultado == true) {
+        header("Location: consultarPeda.php?result=alert");
+    } else {
+        header("Location: formCadItem.php?result=false");
+    }
 }
