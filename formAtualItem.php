51b8c7a11067312cc8ed5e5199664264cc3b247a
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-10-27 12:16:29 -0300

    Update formAtualItem.php

--- formAtualItem.php
+++ formAtualItem.php
@@ -42,13 +42,18 @@
   require_once "conecta.php";
   require_once "topo.html";
 
-    ?>
+  $id = $_GET['idItem'];
+  $sql = "SELECT * FROM item WHERE idItem=$id";
+  $resultado = mysqli_query($conexao, $sql);
+  $linha = mysqli_fetch_array($resultado);
+  mysqli_close($conexao);
+?>
 
 <br><br>
 	<div class="row">
 	
 		<div class="container z-depth-3 col s6 offset-s4 cont1">
-		<h4 class="center-align white-text"> CADASTRAR ITEM </h4>
+		<h4 class="center-align white-text"> ATUALIZAR INFORMAÇÕES ITEM </h4>
 		</div>
 		
 		<div class="col s8 offset-s3">
@@ -56,19 +61,22 @@
 			<div class="container ">
 			<br>
 			<div class="col s10 offset-s1">
-				<form method="POST" action="cadItem.php">
+				<form method="POST" action="atualizarItem.php">
+
+				<input type="hidden" name="idItem" value="<?php echo $linha['idItem']?>">
+
                     <p class="topicos">Nome do Item:</p>
 					<div class="input-field">
 						<i class="material-icons prefix"></i>
 						<label for="name"> </label>
-						<input type="text" name="nomeItem" required>
+						<input type="text" name="nomeItem" value="<?php echo $linha['tipo']; ?>" required>
 					</div>
 
 					<p class="topicos">Descrição:</p>
 					<div class="input-field">
 						<i class="material-icons prefix "></i>
 						<label for="descrição"></label>
-						<input type="text" name="descricao" required>
+						<input type="text" name="descricao" value="<?php echo $linha['descricao']; ?> required>
 					</div>
 					
 					<p class="topicos">Almoxarifado de Destino do Item:</p>
