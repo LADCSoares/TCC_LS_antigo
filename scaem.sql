759723f4b867ddbbcc3072f0f503a73bc37a9501
Luisa Andrielly <145121607+LADCSoares@users.noreply.github.com>
2023-11-10 13:12:47 -0300

    Add files via upload

---
+++ scaem.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 10-Nov-2023 às 15:42
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `scaem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estoque`
+--
+
+DROP TABLE IF EXISTS `estoque`;
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `idEstoque` int NOT NULL AUTO_INCREMENT,
+  `dataRetirada` date NOT NULL,
+  `dataEntrada` date NOT NULL,
+  `quantidade` int NOT NULL,
+  `finalidadeRetirada` text NOT NULL,
+  `idItem` int NOT NULL,
+  `IdFuncionario` int NOT NULL,
+  PRIMARY KEY (`idEstoque`),
+  KEY `IdFuncionario` (`IdFuncionario`),
+  KEY `idItem` (`idItem`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `idFuncionario` int NOT NULL AUTO_INCREMENT,
+  `nome` text NOT NULL,
+  `cpf` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `cargo` varchar(300) NOT NULL,
+  `telefone` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`idFuncionario`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`idFuncionario`, `nome`, `cpf`, `cargo`, `telefone`) VALUES
+(4, 'Carla', '026.025.203-30', 'Professora', '(55)9999-9999');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `idItem` int NOT NULL AUTO_INCREMENT,
+  `nomeItem` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `descricao` text NOT NULL,
+  `almoxarifado` varchar(1) NOT NULL,
+  `quantidade` int NOT NULL,
+  PRIMARY KEY (`idItem`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `item`
+--
+
+INSERT INTO `item` (`idItem`, `nomeItem`, `descricao`, `almoxarifado`, `quantidade`) VALUES
+(1, 'caneta', 'bic', '1', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `escola` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `estoque`
+--
+ALTER TABLE `estoque`
+  ADD CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`IdFuncionario`) REFERENCES `funcionario` (`idFuncionario`),
+  ADD CONSTRAINT `estoque_ibfk_2` FOREIGN KEY (`idItem`) REFERENCES `item` (`idItem`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
